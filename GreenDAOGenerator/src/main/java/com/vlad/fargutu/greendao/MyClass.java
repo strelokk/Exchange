b8cead28d142ade87ee0888cc600dbf3bfd5c4bd
vladfargutu <fargutu.vlad@gmail.com>
2014-03-20 12:53:14 +0200

    added GreenDAOGenerator module for generating db entities

--- GreenDAOGenerator/src/main/java/com/vlad/fargutu/greendao/MyClass.java
+++ GreenDAOGenerator/src/main/java/com/vlad/fargutu/greendao/MyClass.java
@@ -2,15 +2,42 @@ package com.vlad.fargutu.greendao;
 
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
 
 public class MyClass {
 
     public static void main(String args[]) throws Exception {
         Schema schema = new Schema(3, "com.vlad.fargutu.exchange.dao");
-        Entity upload = schema.addEntity("Upload");
-        upload.addIdProperty();
-        upload.addStringProperty("text");
+
+        Entity course = schema.addEntity("Course");
+        course.addIdProperty().notNull();
+        course.addStringProperty("name").notNull();
+        course.addStringProperty("code");
+        course.addByteArrayProperty("icon");
+
+        Entity courseValue = schema.addEntity("CourseValue");
+        courseValue.addIdProperty().notNull();
+        courseValue.addFloatProperty("purchase");
+        courseValue.addFloatProperty("sale");
+        courseValue.addDateProperty("updatedDate");
+        courseValue.addLongProperty("courseId");
+        courseValue.addLongProperty("bankId");
+
+        Entity branch = schema.addEntity("Branch");
+        branch.addIdProperty().notNull();
+        branch.addStringProperty("name").notNull();
+        branch.addLongProperty("latitude");
+        branch.addLongProperty("longitude");
+        branch.addStringProperty("address");
+        Property bankId = branch.addLongProperty("bankId").getProperty();
+
+        Entity bank = schema.addEntity("Bank");
+        bank.addIdProperty().notNull();
+        bank.addStringProperty("name").notNull();
+        bank.addDateProperty("updatedDate");
+        bank.addToMany(branch, bankId);
+
         new DaoGenerator().generateAll(schema, "D:/Development/Contests/Exchange/android-app/src/main/java/");
     }
 
